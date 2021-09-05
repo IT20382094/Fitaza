eec61eca935c50fc8ca56f76ab1511796686056c
JayaminiKMN <navodajayamini567@gmail.com>
2021-09-06 02:27:31 +0530

    Added Workout Design

---
+++ app/src/main/java/com/example/fitazafitnessapp/activity_chest_steps.java
@@ -0,0 +1,14 @@
+package com.example.fitazafitnessapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class activity_chest_steps extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_chest_steps);
+    }
+}
