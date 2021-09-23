c75cc13bf5a81047608fe342eb77947d0a647663
JayaminiKMN <navodajayamini567@gmail.com>
2021-09-24 00:03:44 +0530

    workout insert

--- app/src/main/java/com/example/fitazafitnessapp/WorkoutStepsActivity.java
+++ app/src/main/java/com/example/fitazafitnessapp/WorkoutStepsActivity.java
@@ -1,8 +1,10 @@
 package com.example.fitazafitnessapp;
 
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
 import android.text.TextUtils;
 import android.view.View;
@@ -10,128 +12,106 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import com.example.fitazafitnessapp.db.FirebaseDB;
+import com.example.fitazafitnessapp.model.Workout;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.DatabaseReference;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
+
+
 public class WorkoutStepsActivity extends AppCompatActivity {
 
-    private Button btnSeeSteps,btnNext;
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_workout_steps);
-
-        btnSeeSteps = findViewById(R.id.btn_see_steps);
-        btnSeeSteps.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(WorkoutStepsActivity.this, activity_belly_steps.class);
-                startActivity(intent);
-            }
-        });
-
-        btnNext = findViewById(R.id.btn_next);
-        btnNext.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(WorkoutStepsActivity.this, WorkoutCaloriesActivity.class);
-                startActivity(intent);
-            }
-        });
-    }
-}
-
-
-
-//    EditText date, start_time, target_time;
-//    Workout workoutobj;
 //    private Button btnSeeSteps,btnNext;
-////    DatabaseReference dbRef;
 //
 //    @Override
 //    protected void onCreate(Bundle savedInstanceState) {
 //        super.onCreate(savedInstanceState);
 //        setContentView(R.layout.activity_workout_steps);
 //
-//        date =findViewById(R.id.workout_date);
-//        start_time=findViewById(R.id.workout_start_time);
-//        target_time=findViewById(R.id.workout_end_time);
+//        btnSeeSteps = findViewById(R.id.btn_see_steps);
+//        btnSeeSteps.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                Intent intent = new Intent(WorkoutStepsActivity.this, activity_belly_steps.class);
+//                startActivity(intent);
+//            }
+//        });
 //
-//        btnNext=findViewById(R.id.btn_next);
-////        btn_show=findViewById(R.id.btn_show);
-////        btn_update=findViewById(R.id.btn_update);
-////        btn_delete=findViewById(R.id.btn_delete);
-//
-//        workoutobj =new Workout();
-//
+//        btnNext = findViewById(R.id.btn_next);
 //        btnNext.setOnClickListener(new View.OnClickListener() {
 //            @Override
 //            public void onClick(View view) {
-//
-//                createData();
 //                Intent intent = new Intent(WorkoutStepsActivity.this, WorkoutCaloriesActivity.class);
 //                startActivity(intent);
 //            }
 //        });
-
-//        btn_delete.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View view) {
-//
-//                deleteData();
-//            }
-//        });
-//
-//        btn_show.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View view) {
-//
-//                showData();
-//            }
-//        });
-//        btn_update.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View view) {
-//
-//                updateData();
-//            }
-//        });
 //    }
 
-//    public void clearControls(){
-//
-//        date.setText("");
-//        start_time.setText("");
-//        target_time.setText("");
-//    }
-//
-//    public void createData(){
-//        dbRef = FirebaseDatabase.getInstance().getReference().child("Workout");
-//        try{
-//            if (TextUtils.isEmpty(id.getText().toString()))
-//                Toast.makeText(getApplicationContext(),"Please enter an ID", Toast.LENGTH_SHORT).show();
-//            else if (TextUtils.isEmpty(name.getText().toString()))
-//                Toast.makeText(getApplicationContext(),"Please enter a name", Toast.LENGTH_SHORT).show();
-//            else if (TextUtils.isEmpty(address.getText().toString()))
-//                Toast.makeText(getApplicationContext(),"Please enter an address", Toast.LENGTH_SHORT).show();
-//            else if (TextUtils.isEmpty(contactNumber.getText().toString()))
-//                Toast.makeText(getApplicationContext(),"Please enter a contact number", Toast.LENGTH_SHORT).show();
-//
-//            else{
-//                //Take inputs from user and assign them to this instance (stuBoj) of the student
-//                stuObj.setId(id.getText().toString().trim());
-//                stuObj.setName(name.getText().toString().trim());
-//                stuObj.setAddress(address.getText().toString().trim());
-//                stuObj.setContactNumber(Integer.parseInt(contactNumber.getText().toString().trim()));
-//
-//                //insert it to database
-//                dbRef.push().setValue(stuObj);
-//
-//                dbRef.child("Std3").setValue(stuObj);
-//                //feedback to user via a toast message
-//                Toast.makeText(getApplicationContext(), "Data saved successfully", Toast.LENGTH_SHORT).show();
-//                clearControls();
-//            }
-//        }
-//        catch (NumberFormatException e){
-//            Toast.makeText(getApplicationContext(), "Invalid contact Number", Toast.LENGTH_SHORT).show();
-//        }
-//    }
+
+
+    EditText date, start_time, target_time;
+    private Button btnSeeSteps,btnNext;
+    DatabaseReference dbRef;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_workout_steps);
+
+        date = findViewById(R.id.workout_date);
+        start_time = findViewById(R.id.workout_start_time);
+        target_time = findViewById(R.id.workout_end_time);
+
+        btnNext = findViewById(R.id.btn_next);
+
+        dbRef = FirebaseDB.getFirebaseDatabaseRef();
+
+        btnNext.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                createData();
+                Intent intent = new Intent(WorkoutStepsActivity.this, WorkoutCaloriesActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+
+    public void clearControls(){
+
+        date.setText("");
+        start_time.setText("");
+        target_time.setText("");
+    }
+
+    public void createData(){
+        dbRef = FirebaseDatabase.getInstance().getReference();
+
+        if (TextUtils.isEmpty(date.getText().toString()))
+            Toast.makeText(getApplicationContext(),"Please enter a date", Toast.LENGTH_SHORT).show();
+        else if (TextUtils.isEmpty(start_time.getText().toString()))
+            Toast.makeText(getApplicationContext(),"Please enter a starting time", Toast.LENGTH_SHORT).show();
+        else if (TextUtils.isEmpty(target_time.getText().toString()))
+            Toast.makeText(getApplicationContext(),"Please enter a target time", Toast.LENGTH_SHORT).show();
+
+        else{
+
+            Workout workoutobj = new Workout();
+            workoutobj.setWorkoutDay(date.getText().toString().trim());
+            workoutobj.setWorkoutStartTime(start_time.getText().toString().trim());
+            workoutobj.setWorkoutTargetTime(target_time.getText().toString().trim());
+
+            dbRef.push().setValue(workoutobj);
+
+            //feedback to user via a toast message
+            Toast.makeText(getApplicationContext(), "Data saved successfully", Toast.LENGTH_SHORT).show();
+            clearControls();
+        }
+    }
+}
+
+
+
+
