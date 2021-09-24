d17503034eea50b17ddc830ad9d502b6a75a9e84
SashiniUdyani <sashiniudyani14@gmail.com>
2021-09-24 12:21:25 +0530

    implemented insert

--- app/src/main/java/com/example/fitazafitnessapp/MealDailyPlanActivity.java
+++ app/src/main/java/com/example/fitazafitnessapp/MealDailyPlanActivity.java
@@ -1,5 +1,6 @@
 package com.example.fitazafitnessapp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.DatePickerDialog;
@@ -8,18 +9,34 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.DatePicker;
+import android.widget.LinearLayout;
+
+import com.example.fitazafitnessapp.db.FirebaseDB;
+import com.example.fitazafitnessapp.model.MealPlan;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.ValueEventListener;
 
 public class MealDailyPlanActivity extends AppCompatActivity {
 
     private Button btnDailyPlan;
     private Button btnCalorieCal;
     private Button btnDelete;
-
+    private LinearLayout lyBreakfastMenu1, lyLunchMenu1, lyDinnerMenu1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_meal_daily_plan);
+
+        lyBreakfastMenu1 = findViewById(R.id.lyBreakfastMenu1);
+        lyLunchMenu1 = findViewById(R.id.lyLunchMenu1);
+        lyDinnerMenu1 = findViewById(R.id.lyDinnerMenu1);
+
+        setLayoutsFalse();
+
+        loadMenus();
 
 //        new DatePickerDialog(MealDailyPlanActivity.this, R.style.DialogTheme, new DatePickerDialog.OnDateSetListener() {
 //            @Override
@@ -46,5 +63,38 @@ public class MealDailyPlanActivity extends AppCompatActivity {
             }
         });
 
+    }
+
+    private void setLayoutsFalse() {
+        lyBreakfastMenu1.setVisibility(View.GONE);
+        lyLunchMenu1.setVisibility(View.GONE);
+        lyDinnerMenu1.setVisibility(View.GONE);
+    }
+
+    private void loadMenus() {
+        DatabaseReference mealPlan = FirebaseDB.getFirebaseDatabaseRef().child("mealPlan");
+        mealPlan.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                for (DataSnapshot dataSnapshot : snapshot.getChildren()) {
+                    MealPlan value = dataSnapshot.getValue(MealPlan.class);
+                    if (value.getMealPlanName().equals("BreakfastMenu1")) {
+                        lyBreakfastMenu1.setVisibility(View.VISIBLE);
+                    }
+                    if (value.getMealPlanName().equals("BreakfastMenu2")){
+                        lyLunchMenu1.setVisibility(View.VISIBLE);
+                    }
+                    if (value.getMealPlanName().equals("BreakfastMenu3")){
+                        lyDinnerMenu1.setVisibility(View.VISIBLE);
+                    }
+
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
     }
 }
---
+++ app/src/main/java/com/example/fitazafitnessapp/model/MealPlan.java
@@ -0,0 +1,128 @@
+package com.example.fitazafitnessapp.model;
+
+import java.util.List;
+
+public class MealPlan {
+
+    private String mealPlanId;
+    private String mealPlanName;
+
+    public MealPlan() {
+    }
+
+    public MealPlan(String mealPlanId, String mealPlanName) {
+        this.mealPlanId = mealPlanId;
+        this.mealPlanName = mealPlanName;
+    }
+
+    public String getMealPlanId() {
+        return mealPlanId;
+    }
+
+    public void setMealPlanId(String mealPlanId) {
+        this.mealPlanId = mealPlanId;
+    }
+
+    public String getMealPlanName() {
+        return mealPlanName;
+    }
+
+    public void setMealPlanName(String mealPlanName) {
+        this.mealPlanName = mealPlanName;
+    }
+
+    //    List<String> calPlan1200Breakfast;
+//    List<String> calPlan1200Lunch;
+//    List<String> calPlan1200Dinner;
+//    List<String> calPlan1300Breakfast;
+//    List<String> calPlan1300Lunch;
+//    List<String> calPlan1300Dinner;
+//    List<String> calPlan1500Breakfast;
+//    List<String> calPlan1500Lunch;
+//    List<String> calPlan1500Dinner;
+//
+//
+//    public MealPlan() {
+//    }
+//
+//    public List<String> getCalPlan1200Breakfast() {
+//        return calPlan1200Breakfast;
+//    }
+//
+//    public Object setCalPlan1200Breakfast(List<String> calPlan1200Breakfast) {
+//        this.calPlan1200Breakfast = calPlan1200Breakfast;
+//
+//        return null;
+//    }
+//
+//    public List<String> getCalPlan1200Lunch(List<String> calPlan1200Lunch) {
+//        return this.calPlan1200Lunch;
+//    }
+//
+//    public Object setCalPlan1200Lunch(List<String> calPlan1200Lunch) {
+//        this.calPlan1200Lunch = calPlan1200Lunch;
+//        return null;
+//    }
+//
+//    public List<String> getCalPlan1200Dinner() {
+//        return calPlan1200Dinner;
+//    }
+//
+//    public Object setCalPlan1200Dinner(List<String> calPlan1200Dinner) {
+//        this.calPlan1200Dinner = calPlan1200Dinner;
+//        return null;
+//    }
+//
+//    public List<String> getCalPlan1200Lunch() {
+//        return calPlan1200Lunch;
+//    }
+//
+//    public List<String> getCalPlan1300Breakfast() {
+//        return calPlan1300Breakfast;
+//    }
+//
+//    public List<String> setCalPlan1300Breakfast(List<String> calPlan1300Breakfast) {
+//        this.calPlan1300Breakfast = calPlan1300Breakfast;
+//        return calPlan1300Breakfast;
+//    }
+//
+//    public List<String> getCalPlan1300Lunch() {
+//        return calPlan1300Lunch;
+//    }
+//
+//    public void setCalPlan1300Lunch(List<String> calPlan1300Lunch) {
+//        this.calPlan1300Lunch = calPlan1300Lunch;
+//    }
+//
+//    public List<String> getCalPlan1300Dinner() {
+//        return calPlan1300Dinner;
+//    }
+//
+//    public void setCalPlan1300Dinner(List<String> calPlan1300Dinner) {
+//        this.calPlan1300Dinner = calPlan1300Dinner;
+//    }
+//
+//    public List<String> getCalPlan1500Breakfast() {
+//        return calPlan1500Breakfast;
+//    }
+//
+//    public void setCalPlan1500Breakfast(List<String> calPlan1500Breakfast) {
+//        this.calPlan1500Breakfast = calPlan1500Breakfast;
+//    }
+//
+//    public List<String> getCalPlan1500Lunch() {
+//        return calPlan1500Lunch;
+//    }
+//
+//    public void setCalPlan1500Lunch(List<String> calPlan1500Lunch) {
+//        this.calPlan1500Lunch = calPlan1500Lunch;
+//    }
+//
+//    public List<String> getCalPlan1500Dinner() {
+//        return calPlan1500Dinner;
+//    }
+//
+//    public void setCalPlan1500Dinner(List<String> calPlan1500Dinner) {
+//        this.calPlan1500Dinner = calPlan1500Dinner;
+//    }
+}
